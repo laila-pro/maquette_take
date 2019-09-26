ffcd416df3fe88a723e782a7e0d7347713422f9a
ErwannLG <e.legall@codeur.online>
2019-09-26 10:54:52 +0200

    admin indentation

--- admin.php
+++ admin.php
@@ -1,14 +1,13 @@
-<?php
-require 'header.php';?>
+<?php require 'header.php';?>
 <div class="row">
-<div class="col-3">
-<?php require 'sidebar.php';?>
-</div>
-<div class="col-9">
-<?php if (isset($_GET['voir']))
-require 'article.php';
-?>
-</div>
+  <div class="col-3">
+  <?php require 'sidebar.php';?>
+  </div>
+  <div class="col-9">
+  <?php if (isset($_GET['voir']))
+  require 'article.php';
+  ?>
+  </div>
 </div>
 <?php
 require 'footer.php';
--- sidebar.php
+++ sidebar.php
@@ -1,69 +1,62 @@
-
-
-
 <div id="content">
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-
-<div class="wrapper">
-    <!-- Sidebar -->
-    <nav id="sidebar">
-        <div class="sidebar-header">
+      <div class="wrapper">
+        <!-- Sidebar -->
+        <nav id="sidebar">
+          <div class="sidebar-header">
             <h3>Maquette take</h3>
-        </div>
+          </div>
 
-        <ul class="list-unstyled components">
+          <ul class="list-unstyled components">
             <p>BACK-OFFICE</p>
             <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">ARTICLE</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <li>
-                        <a href="admin.php?voir=1">Ajouter</a>
-                    </li>
-                    <li>
-                        <a href="#">Modifier</a>
-                    </li>
-                    <li>
-                        <a href="#">Supprimer</a>
-                    </li>
-                </ul>
+              <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">ARTICLE</a>
+              <ul class="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                  <a href="admin.php?voir=1">Ajouter</a>
+                </li>
+                <li>
+                  <a href="#">Modifier</a>
+                </li>
+                <li>
+                  <a href="#">Supprimer</a>
+                </li>
+              </ul>
             </li>
 
             <li>
-                <a href="#voyageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Voyage</a>
-                <ul class="collapse list-unstyled" id="voyageSubmenu">
-                    <li>
-                        <a href="#">Ajouter</a>
-                    </li>
-                    <li>
-                        <a href="#">Modifier</a>
-                    </li>
-                    <li>
-                        <a href="#">Supprimer</a>
-                    </li>
-                </ul>
+              <a href="#voyageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Voyage</a>
+              <ul class="collapse list-unstyled" id="voyageSubmenu">
+                <li>
+                  <a href="#">Ajouter</a>
+                </li>
+                <li>
+                  <a href="#">Modifier</a>
+                </li>
+                <li>
+                  <a href="#">Supprimer</a>
+                </li>
+              </ul>
             </li>
             <li>
-                <a href="#userSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">UTILISATEUR</a>
-                <ul class="collapse list-unstyled" id="userSubmenu">
-                    <li>
-                        <a href="#">Ajouter</a>
-                    </li>
-                    <li>
-                        <a href="#">Modifier</a>
-                    </li>
-                    <li>
-                        <a href="#">Supprimer</a>
-                    </li>
-                </ul>
+              <a href="#userSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">UTILISATEUR</a>
+              <ul class="collapse list-unstyled" id="userSubmenu">
+                <li>
+                  <a href="#">Ajouter</a>
+                </li>
+                <li>
+                  <a href="#">Modifier</a>
+                </li>
+                <li>
+                  <a href="#">Supprimer</a>
+                </li>
+              </ul>
             </li>
-            </ul>
-    </nav>
+          </ul>
+        </nav>
 
+      </div>
+    </div>
+  </nav>
 </div>
-</div>
-</nav>
-
-</div>
-
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
