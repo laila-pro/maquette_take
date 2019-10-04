736c1d8e3c9ee9738217db76e091b73b7d9fd684
Erwann Le Gall <e.legall@codeur.online>
2019-10-04 10:42:31 +0200

    admin.php security & add article image fix

--- article_save.php
+++ article_save.php
@@ -1,16 +1,18 @@
-
 <?php
-$file = $_FILES['filepond'];
-require 'connexion.php';
-$tab = array(
-  'titre' => $_POST['titre'],
-  'contenu' => $_POST['contenu'],
-  'image'=>$file["name"]);
-
-//création de la requête SQL:
-$sql = 'INSERT INTO `article` (`titre`, `contenu`, `image`, `auteur`, `date`) VALUES ("'.$tab["contenu"].'", "'.$tab["titre"].'","'.$tab["image"].'","test",CURDATE());';
-move_uploaded_file($file["tmp_name"], './upload/' .$file["name"]);
-// echo $sql;
-$conn->exec($sql);
-header('Location: admin.php?choix=0');
+$result = move_uploaded_file ( $_FILES["filepond"]["tmp_name"] , "upload/". $_FILES["filepond"]["name"] );
+if ($result) {
+  require 'connexion.php';
+  $tab = array(
+    'titre' => $_POST['titre'],
+    'contenu' => $_POST['contenu'],
+    'image'=> $_FILES["filepond"]["name"] );
+  //création de la requête SQL:
+  $sql = 'INSERT INTO `article` (`titre`, `contenu`, `image`, `auteur`, `date`) VALUES ("'.$tab["contenu"].'", "'.$tab["titre"].'","'.$tab["image"].'","test",CURDATE());';
+  // echo $sql;
+  $conn->exec($sql);
+    header('Location: admin.php?choix=0');
+}
+else {
+  echo "Le fichier n'a pu être téléchargé";
+}
 ?>
